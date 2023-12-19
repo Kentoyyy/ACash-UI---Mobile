a15072b541f565115dff73dd562ed8c3380ec07e
_Kentoyyy <kenalcantara55@gmail.com>
2023-12-20 07:48:22 +0800

    ups

---
+++ app/src/main/java/com/example/acash/Amain.java
@@ -0,0 +1,14 @@
+package com.example.acash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Amain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_amain);
+    }
+}
